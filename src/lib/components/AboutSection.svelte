8d798ddf4aba606c0ec18bc3cc6e7849b2b70105
Lucas Vogel <contact@vogelino.com>
2025-07-16 13:51:48 +0200

    feat(AboutSection): Improve text size and team Foto

--- src/lib/components/AboutSection.svelte
+++ src/lib/components/AboutSection.svelte
@@ -15,54 +15,81 @@
 	<h2 class="font-headings font-bold text-balance">
 		{aboutPage.metadata.title}
 	</h2>
-	<figure class="group flex flex-col gap-2">
-		<enhanced:img
-			src={arohLeads}
-			alt="a-roh lead architects"
-			class="max-h-[50svh] w-full object-contain sm:aspect-square"
-		/>
-		<figcaption
-			class={cn(
-				'text-muted-foreground text-center text-xs tracking-tight opacity-50 transition-opacity',
-				'group-hover:opacity-100'
-			)}
-		>
-			© {aboutPage.metadata.photoCopyright}
-		</figcaption>
-	</figure>
+	<enhanced:img
+		src={arohLeads}
+		alt="a-roh lead architects"
+		class="max-h-[50svh] w-full object-contain sm:aspect-square"
+		sizes="160px"
+	/>
 	<div class="pt-8 sm:contents">
 		<div class="flex flex-col justify-between gap-6 lg:col-span-3">
 			<div
 				class={cn(
-					'[&>p]:text-pretty',
-					'[&>h1]:font-headings [&>h1]:mt-4 [&>h1]:font-bold [&>h1]:text-balance [&>h1]:first:mt-0',
-					'[&>h2]:font-headings [&>h2]:mt-4 [&>h2]:font-bold [&>h2]:text-balance [&>h2]:first:mt-0',
-					'[&>h3]:font-headings [&>h3]:mt-4 [&>h3]:font-bold [&>h3]:text-balance [&>h3]:first:mt-0',
-					'[&>h4]:font-headings [&>h4]:mt-4 [&>h4]:font-bold [&>h4]:text-balance [&>h4]:first:mt-0',
-					'flex flex-col gap-2'
+					'text-sm [&>p]:text-pretty',
+					'[&>h1]:font-headings [&>h1]:mt-4 [&>h1]:text-base [&>h1]:font-bold [&>h1]:text-balance [&>h1]:first:mt-0',
+					'[&>h2]:font-headings [&>h2]:mt-4 [&>h2]:text-base [&>h2]:font-bold [&>h2]:text-balance [&>h2]:first:mt-0',
+					'[&>h3]:font-headings [&>h3]:mt-4 [&>h3]:text-base [&>h3]:font-bold [&>h3]:text-balance [&>h3]:first:mt-0',
+					'[&>h4]:font-headings [&>h4]:mt-4 [&>h4]:text-base [&>h4]:font-bold [&>h4]:text-balance [&>h4]:first:mt-0',
+					'[&_a]:decoration-muted-foreground/25 [&_a]:hover:decoration-foreground [&_a]:underline',
+					'[&_a]:focus-visible:outline-foreground [&_a]:underline-offset-2 [&_a]:transition-colors',
+					'[&_li]:text-balance [&_ul]:flex [&_ul]:flex-col [&_ul]:gap-1',
+					'flex flex-col gap-2.5'
 				)}
 			>
 				<aboutPage.Content />
+
+				<p class="mt-2">©&thinsp;{new Date().getFullYear()} - a-roh - all rights reserved</p>
 			</div>
 		</div>
-		<div class="flex flex-col gap-6 pt-8 lg:col-span-2">
+		<div class="flex flex-col gap-5 pt-8 text-sm lg:col-span-2">
 			<ul>
 				{#each aboutPage.metadata.emails as email}
-					<li><a href="mailto:{email}">{email}</a></li>
+					<li>
+						<a
+							href="mailto:{email}"
+							itemprop="email"
+							class={cn(
+								'decoration-muted-foreground/25 hover:decoration-foreground underline underline-offset-2 transition-colors',
+								'focus-visible:outline-foreground focus-visible:decoration-foreground focus-visible:underline-offset-2'
+							)}
+						>
+							{email}
+						</a>
+					</li>
 				{/each}
 			</ul>
 			<ul>
 				{#each aboutPage.metadata.phones as phone}
-					<li><a href="tel:{phone}">{phone}</a></li>
+					<li>
+						<a
+							href="tel:{phone}"
+							itemprop="telephone"
+							class={cn(
+								'decoration-muted-foreground/25 hover:decoration-foreground underline underline-offset-2 transition-colors',
+								'focus-visible:outline-foreground focus-visible:decoration-foreground focus-visible:underline-offset-2'
+							)}
+						>
+							{phone}
+						</a>
+					</li>
 				{/each}
 			</ul>
 			<ul class="flex flex-col gap-6">
 				{#each aboutPage.metadata.addresses as address}
-					<li>
-						<strong class="font-headings">{address.label}</strong><br />
-						{address.line1}<br />
-						{address.line2}
-					</li>
+					<a
+						href={`https://www.google.com/maps/search/?api=1&query=${address.line1},${address.line2}`}
+						target="_blank"
+						rel="noopener noreferrer"
+						itemscope
+						itemtype="https://schema.org/PostalAddress"
+						class={cn(
+							'decoration-muted-foreground/25 hover:decoration-foreground underline underline-offset-2 transition-colors',
+							'focus-visible:outline-foreground focus-visible:decoration-foreground focus-visible:underline-offset-2'
+						)}
+					>
+						<span itemprop="streetAddress">{address.line1}</span><br />
+						<span itemprop="addressLocality">{address.line2}</span>
+					</a>
 				{/each}
 			</ul>
 			<ul>
@@ -70,7 +97,11 @@
 					<li>
 						<a
 							href={social.url}
-							class="decoration-muted-foreground hover:decoration-foreground underline underline-offset-2 transition-colors"
+							itemprop="sameAs"
+							class={cn(
+								'decoration-muted-foreground/25 hover:decoration-foreground underline underline-offset-2 transition-colors',
+								'focus-visible:outline-foreground focus-visible:decoration-foreground focus-visible:underline-offset-2'
+							)}
 						>
 							{social.label}
 						</a>
